27a346343348fdcda1dbc83b5be775246ce2c22f
group11finalyearproject-max <group11finalyearproject@gmail.com>
2026-05-05 06:19:11 -0700

    approved

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+        plugins: [
             "nativewind/babel",
+            // Required for stable Android release bundles when Reanimated/Worklets are installed.
+            "react-native-reanimated/plugin",
         ],
     };
 };
